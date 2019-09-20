d6266d95c038da042b84c1f5613e1c2cad50b242
Anna Sh <anna.sh.pb@gmail.com>
2019-09-20 18:48:26 +0300

    task03

---
+++ js/task-03.js
@@ -0,0 +1,40 @@
+"use strict";
+
+function findBestEmployee(employees) {
+  let max = 0;
+  let name;
+  const keys = Object.keys(employees);
+  for (const key of keys) {
+    if (max < employees[key]) {
+      max = employees[key];
+      name = key;
+    }
+  }
+  return name;
+}
+
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99
+  })
+);
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4
+  })
+);
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38
+  })
+);
