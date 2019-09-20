7253958c15a632313f29cdade64c449169766c46
Anna Sh <anna.sh.pb@gmail.com>
2019-09-20 18:35:36 +0300

    task02

---
+++ js/task-02.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function countProps(obj) {
+    let keys = Object.keys(obj);
+    return keys.length;
+}
+
+console.log(countProps({}));
+console.log(countProps({ name: 'Mango', age: 2 }));
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
