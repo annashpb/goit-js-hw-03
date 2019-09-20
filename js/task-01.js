0d09e9f773c4e970532656cf6de369dfec97cd25
Anna Sh <anna.sh.pb@gmail.com>
2019-09-20 18:28:09 +0300

    task01

---
+++ js/task-01.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const user = {
+  name: "Mango",
+  age: 20,
+  hobby: "html",
+  premium: true
+};
+
+user.mood = "happy";
+user.hobby = "javascript";
+user.premium = false;
+
+const keys = Object.keys(user);
+for (let key of keys) {
+  console.log(`${key}: ${user[key]}`);
+}
